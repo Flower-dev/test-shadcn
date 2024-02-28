7cb38d78de543214a46d31cf158b9486d4e3289b
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2024-02-28 22:36:49 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { RocketIcon } from "@radix-ui/react-icons"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+     <Alert>
+     <RocketIcon className="h-4 w-4" />
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+     </Alert>
+     <Button>Button</Button>
     </main>
   );
 }
